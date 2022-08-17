da92b2c0112a1c7efcc9f20fec3787e8e6e9ce37
ClementNjeru <njeruclement36@gmail.com>
2022-08-17 22:36:53 +0300

    done

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+def talk
+    puts "Hello World!"
+    # puts"The Person is walking"
+    end
+    def walk
+        puts"The Person is walking"
+    end
+end
+
+
+Mary= Person.new
+Mary.talk
+Mary.walk
+
+
