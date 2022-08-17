d1847d09af54ee3765b4218b361b2a7c8bbf3499
ClementNjeru <njeruclement36@gmail.com>
2022-08-17 22:36:44 +0300

    commit tree 99642fdffe685816627aede733a092a3c1afa46c

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+end
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
+
+class Dog
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+tusker= Dog.new
+tusker.sit
